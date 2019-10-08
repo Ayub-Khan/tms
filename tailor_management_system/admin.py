b0c080038c7d5baa1ca119200bf1f35ed84fbafd
Saad.Ali <saad@A002-00017.arbisoft.com>
2019-10-08 15:56:48 +0500

    Added new project with the right name.
    1. Removed all test-quality-errors.

---
+++ tailor_management_system/admin.py
@@ -0,0 +1,5 @@
+"""Driver function."""
+
+from django.contrib import admin
+
+# Register your models here.
---
+++ tailor_management_system/apps.py
@@ -0,0 +1,9 @@
+"""Driver function."""
+
+from django.apps import AppConfig
+
+
+class TailorManagementSystemConfig(AppConfig):
+    """Driver function."""
+
+    name = 'tailor_management_system'
