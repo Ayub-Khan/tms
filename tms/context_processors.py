95c3f758f08f3a11aed98126de796a5658aca8d8
Saad Ali <saadali5997@gmail.com>
2019-10-30 11:50:49 +0500

    Render Title Using Custom Tag (#13)

--- tms/context_processors.py
+++ tms/context_processors.py
@@ -6,5 +6,4 @@ from django.urls import resolve
 def template_title(request):
     """Add template title to template context."""
     url_name = resolve(request.path_info).url_name
-    page_title = url_name.replace('_', ' ').title()
-    return {'title': page_title}
+    return {'title': url_name}
