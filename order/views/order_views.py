5e0c45f063d569722ee2e8bd4e7efe388bf3e199
Saad Ali <saadali5997@gmail.com>
2019-11-11 17:45:33 +0500

    Order done.

--- order/views/order_views.py
+++ order/views/order_views.py
@@ -47,9 +47,10 @@ class OrderAddView(LoginRequiredMixin, View):
 
     def get(self, request, client_id):
         """Return add new order form."""
+        client = get_object_or_404(Client, id=client_id)
         form = OrderForm()
         return render(request, 'order/add-order.html',
-                      {'form': form, 'func': 'Add'})
+                      {'form': form, 'func': 'Add', 'client': client})
 
     def post(self, request, client_id):
         """Save order and redirect to order list."""
@@ -61,7 +62,7 @@ class OrderAddView(LoginRequiredMixin, View):
             new_order.save()
             return redirect('order:orders')
         else:
-            return render(request, 'order/add-order.html', {'form': form, 'func': 'Add'})
+            return render(request, 'order/add-order.html', {'form': form, 'func': 'Add', 'client': client})
 
 
 order_add_view = OrderAddView.as_view()
@@ -75,7 +76,7 @@ class OrderUpdateView(LoginRequiredMixin, View):
         order = get_object_or_404(Order, id=id)
         form = OrderForm(instance=order)
         return render(request, 'order/add-order.html',
-                      {'form': form, 'func': 'Update'})
+                      {'form': form, 'func': 'Update', 'order': order})
 
     def post(self, request, id):
         """Save order and redirect to order list."""
@@ -88,7 +89,7 @@ class OrderUpdateView(LoginRequiredMixin, View):
             new_order.save()
             return redirect('order:orders')
         else:
-            return render(request, 'order/add-order.html', {'form': form, 'func': 'Update'})
+            return render(request, 'order/add-order.html', {'form': form, 'func': 'Update', 'order': order})
 
 
 order_update_view = OrderUpdateView.as_view()
